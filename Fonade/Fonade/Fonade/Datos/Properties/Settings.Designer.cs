76c0f2d3f5317ce3128b8808411a3d0c7e29a623
Luis <lars55360@gmail.com>
2014-10-14 00:58:33 -0500

    productin IP changes

--- Fonade/Fonade/Fonade/Datos/Properties/Settings.Designer.cs
+++ Fonade/Fonade/Fonade/Datos/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace Datos.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=10.33.0.200;Initial Catalog=Fonade2;User ID=desarrollo;Password=Admi" +
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=206.49.179.6;Initial Catalog=Fonade2;User ID=desarrollo;Password=Admi" +
             "n1234;")]
         public string FonadeConnectionString {
             get {
